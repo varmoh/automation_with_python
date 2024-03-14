8ba033228bdd1d59d108289ab262286d556b0744
Varmo <101868197+varmoh@users.noreply.github.com>
2024-03-14 21:46:08 +0200

    First

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # automation_with_python
 Esmassed skripti draftid automatsiooni ja testimise jaoks
+
+#### Struktuur
+    helpers/ 
+        Siia alla tulevad helper scriptid. Ehk siis vajalikud skriptid, mis tuleb jooksutada enne tet skriptide käivitamist.
+        Esialgu on vaid pakkide installimine
---
+++ helpers/install_packages.py
@@ -0,0 +1,46 @@
+import subprocess
+
+# List of packages to be installed
+required_packages = [
+    "unittest",
+    "requests",
+    "threading",
+    "time",
+    "docker"
+]
+
+def install_packages(package_list):
+    installed_packages = []
+    for package in package_list:
+        try:
+            subprocess.check_call(['pip', 'install', package])
+            installed_packages.append(package)
+        except subprocess.CalledProcessError:
+            print(f"Error installing {package}.")
+    
+    return installed_packages
+
+def check_install_imports():
+    installed_packages = []
+    missing_packages = [package for package in required_packages if not package_exists(package)]
+    
+    if missing_packages:
+        print("The following packages are missing and will be installed:")
+        print(missing_packages)
+        installed_packages = install_packages(missing_packages)
+    
+    if installed_packages:
+        print("The following packages were installed:")
+        print(installed_packages)
+    else:
+        print("All required packages were already installed.")
+
+def package_exists(package):
+    try:
+        __import__(package)
+        return True
+    except ImportError:
+        return False
+
+if __name__ == "__main__":
+    check_install_imports()
